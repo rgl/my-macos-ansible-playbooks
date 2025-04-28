4df78976ed91bb6950a1ae66991f70bfb43e866a
Rui Lopes <rgl@ruilopes.com>
2025-04-28 22:10:04 +0000

    install visual studio code

---
+++ roles/desktop/files/install-code-extension.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#eval "$(/usr/local/bin/brew shellenv)"
+export PATH="/usr/local/bin:$PATH"
+set -euxo pipefail
+
+# TODO add support for upgrading the extension to the latest version.
+
+expected="$CODE_EXTENSION_NAME"
+
+# bail when the extension is already installed.
+# NB we could also use --show-versions to include the version as @VERSION suffix.
+# NB extension ids are case-insensitive.
+code --list-extensions | while read actual; do
+    if [ "${actual,,}" == "${expected,,}" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+done
+
+# install.
+code --install-extension "$expected"
